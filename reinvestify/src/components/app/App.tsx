4d8b74fbdfebe48b0595bd14df1a4a551583ee0f
Tim Park <timpark0807@gmail.com>
2020-09-03 21:49:57 -0700

    Fetch using promise.all

--- reinvestify/src/components/app/App.tsx
+++ reinvestify/src/components/app/App.tsx
@@ -30,44 +30,117 @@ import Calculator from '../calculator/calculator'
 // };
 
 interface Book {
-  name: string
-  index: number
+  name: string;
+  index: number;
+}
+
+interface User {
+  gender: string; 
+  cell: string; 
+}
+
+interface ToDo {
+  userId: number; 
+  title: string;
+  completed: boolean; 
 }
 
 const App = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [user, setUser] = useState<User>({gender:'', cell:''});
+  const [todo, setTodo] = useState<ToDo>({userId:0, title:'', completed:false})
+  const [loading, setLoading] = useState(true) 
 
   const fetchBooks = async () => {
-    const response: Response = await fetch('https://www.anapioficeandfire.com/api/books')
-    //const json: Promise<any> = await response.json();
-    setBooks(await response.json());
+    const response = await fetch('https://www.anapioficeandfire.com/api/books')
+    const json = await response.json()
+    setBooks(json);
   }
 
-  const fetchThese = async () => {
-    const response: Response = await fetch('https://www.anapioficeandfire.com/api/books')
-    const body: any = await response.json()
-    console.log(body) 
-    return body 
+  const fetchTodo = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const json = await response.json()
+    setTodo(json)
   }
-  
-  const fetchTheseAsync = async () => {
-    const response = fetch('https://www.anapioficeandfire.com/api/books')
-    const response2 = fetch('https://randomuser.me/api/')
-    const output = await Promise.all([response, response2])   
-    console.log(output)
-    return output 
+
+  const fetchUser = async () => {
+    const response = await fetch('https://randomuser.me/api/')
+    const json = await response.json()
+    setUser(json.results[0])
+
+  }
+
+  const fetchEachAsync = async () => {
+    const response =  await fetch('https://www.anapioficeandfire.com/api/books')
+    setBooks(await response.json())
+
+    const response2 = await fetch('https://randomuser.me/api/')
+    setUser((await response2.json()).results[0])
+
+    const response3 = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    setTodo(await response3.json())
+  }
+
+  const fetchPromiseAll = async () => {
+    try {
+      setBooks([])
+      const response = fetch('https://www.anapioficeandfire.com/api/books')
+      const response2 = fetch('https://randomuser.me/api/')
+      const response3 = fetch('https://jsonplaceholder.typicode.com/todos/1')
+      const [bookResponse, userResponse, todoResponse] = await Promise.all([response, response2, response3])   
+      const [bookJSON, userJSON, todoJSON] = await Promise.all([bookResponse.json(), userResponse.json(), todoResponse.json()])
+      setBooks(bookJSON)
+      setTodo(todoJSON)
+      setUser({gender: userJSON.results[0].gender, 
+              cell: userJSON.results[0].cell})
+      setLoading(false)
+    } catch(err) {
+      console.warn(err)
+    }
+  }
+
+  const fetchPromiseAllCleanedUp = async () => {
+    try { 
+      setLoading(true)
+      await Promise.all([
+                        fetchTodo(), 
+                        fetchBooks(), 
+                        fetchUser()
+                      ])
+      setLoading(false)
+    } catch(err) {
+      console.error(err)
+    }
+
   }
 
   return (
     <div className="App">
-      <button onClick={fetchBooks}>Search</button>
-       {books.map((book, index) => {
+
+    <button onClick={fetchBooks}>Books</button>
+    <button onClick={fetchTodo}>ToDo</button>
+    <button onClick={fetchUser}>User</button>
+
+    <br/>
+    <br/>
+
+    <button onClick={fetchEachAsync}>Fetch Each</button>
+    <button onClick={fetchPromiseAllCleanedUp}>Promise All</button>
+
+    {books.map((book, index) => {
          const indexToDisplay = index += 1;
          return <div key={`book${index}`}>{indexToDisplay}&nbsp;{book.name}</div>
        })}
 
-    <button onClick={fetchThese}>Console</button>
-    <button onClick={fetchTheseAsync}>Seperate</button>
+    <br/>
+
+    { loading ? <a>Loading Component</a> : <a>Not Loading</a> }
+
+    <a>{user.gender}</a>
+
+    <br/>
+    <a>{todo.title}</a>
+
     </div>
   );
 };
