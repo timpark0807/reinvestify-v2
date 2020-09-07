0eb7b4f1142c5892bc55a8e18e999d0ba83fa015
Tim Park <timpark0807@gmail.com>
2020-09-06 20:15:29 -0700

    Again

--- reinvestify/src/components/app/App.tsx
+++ reinvestify/src/components/app/App.tsx
@@ -3,92 +3,104 @@ import './App.css';
 import Header from '../header/header'
 import Sidebar from '../sidebar/sidebar'
 import Calculator from '../calculator/calculator'
+import Spinner from 'react-bootstrap/Spinner'
+import { defaultCoreCipherList } from 'constants';
+
+/*
 
 
-interface Person { 
-  gender: string;
-  cell: string; 
+  client ---- server/api ----- database 
+    <-
+
+High Level Steps and Concepts   
+
+  1. Send the request for data 
+      from client to server  
+      client = our users browser
+      server = our backend, an api endpoint 
+
+      request -> HTTP -> Hypertext Transfer Protocol
+        Allows computers and services to talk to each other 
+        Different verbs. Use GET 
+        Send our parameters through the query string parameters 
+
+  2. Receive the response and process it 
+      Typically our response will contain information such as header, status code, 
+      What we REALLY care about is the body
+      we need a way to parse the response and cast the data inside the body into an object 
+
+  3. Display the data to the end user 
+
+
+Scoping the Steps 
+
+Step 1. API Endpoint
+  What are our parameters ? 
+  What is the end point ? 
+  // https://api.airtable.com/blocks/<id> 
+
+
+Step 2. Response
+  What is the structure of the response we are receiving? 
+  What does it look like ? 
+
+
+interface User {
+  name: string;
+  id: number;
 }
 
-interface ToDo {
-  userId: number; 
-  title: string;
-  completed: boolean; 
+interface Block {
+  data: string; 
 }
 
-interface Book {
-  title: string;
-}
+// 3. Authentication
+// API Key. Can be sent via Query String Parameter of 
+
+
+write out the function fetchBlockData
+
+walk through what would happen in syncronous languages
+  - call stack
+  - downside of blocking code imagine if fetch took 30 seconds 
+
+walk through why this wouldn't work in javascript 
+  - call stack
+  - web apis
+  - event queue
+  - event loop 
+  - pros: fetch isn't blocking the main thread  
+  - cons: we're going to throw an error 
+
+introduce Promises/async await keywords 
+  - best of both worlds
+  - pauses our function on the await line until fetch resolves 
+  - doesn't block our main thread so user can use the site 
+
+call             fetchBlockData() 
+stack   = [      main()             ] 
+ 
+event loop    <---> 
+
+event queue = [    fetch()      ]
+
+web apis  = [      ]
+
+*/
 
 
 const App = () => {
 
-  const [loading, setLoading] = useState(false) 
-  const [person, setPerson] = useState<Person>({gender:'', cell:''}); 
-  const [todo, setTodo] = useState<ToDo>({userId:0, title:'', completed:false})
-  const [books, setBooks] = useState<Book[]>([])
-
-  const fetchPerson = async () => {
-    const response = await fetch('https://randomuser.me/api/');
-    const json = await response.json();
-    setPerson(json.results[0])
-  }
-
-  const fetchTodo = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const json = await response.json();
-    setTodo(json)
-  }
-
-  const fetchBooks = async () => {
-    const response = await fetch('https://www.anapioficeandfire.com/api/books')
-    const json = await response.json();
-    setBooks(json)
-  }
-  const getJson = async (url: any): Promise<any> => {
-    const response = await fetch(url)
-    return await response.json()
-  }
-
-  const fetchAll = async () => {
-    try { 
-      setLoading(true)
-      const [p ,t, b] = await Promise.all([
-                                        getJson('https://randomuser.me/api/'), 
-                                        getJson('https://jsonplaceholder.typicode.com/todos/1'),
-                                        getJson('https://www.anapioficeandfire.com/api/books')
-                                      ])
-      setPerson(p.results[0])
-      setTodo(t) 
-      setBooks(b)
-      console.log(books)
-      setLoading(false)      
-      const newPerson: Person = {gender:p.results[0].gender, cell:p.results[0].cell}
-      console.log(newPerson)
-    } catch(error) {
-      console.error(error)
-    }
-  }
-
-  const clear = async () => {
-      setBooks([])
-      setTodo({userId:0, title:'', completed:false})
-      setPerson({gender:'', cell:''})
+  const fetchBlockData = async () => {
+    const response = await fetch('https://randomuser.me/api/')
+    const json = await response.json()
   }
 
   return (
-      <div>
-        <button onClick={clear}>Clear</button>
-        <button onClick={fetchTodo}>Get Todo</button>
-        <button onClick={fetchPerson}>Get Person</button>
-        <button onClick={fetchAll}>Get All</button> 
-        <br/>
-          {loading ? <a>Loading</a> :  <a>{todo.title} {person.gender}</a> }
-
-          { books.length != 0 ? <a>{books[0].title}</a> : <a></a>}
-
-      </div>
-
+    <div>
+          <button onClick={fetchBlockData}>This</button>
+    </div>
   )
+
 }
 export default App;
