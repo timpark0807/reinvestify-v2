fbed1fdefeba8b73475bdac0e490973e306d62c6
Tim Park <timpark0807@gmail.com>
2020-09-07 21:49:17 -0700

    Again

--- reinvestify/src/components/app/App.tsx
+++ reinvestify/src/components/app/App.tsx
@@ -5,16 +5,271 @@ import Sidebar from '../sidebar/sidebar'
 import Calculator from '../calculator/calculator'
 import Spinner from 'react-bootstrap/Spinner'
 import { defaultCoreCipherList } from 'constants';
+import { request } from 'https';
+import { getHeapCodeStatistics } from 'v8';
+import { userInfo } from 'os';
+import { findRenderedDOMComponentWithTag } from 'react-dom/test-utils';
+
+// interface Person {
+//   userId: number;
+//   title: string;
+// }
+
+// const App = () => {
+//   const [person, setPerson] = useState<Person>({'userId':0, 'title':''});
+
+//   const fetchPerson = async () => {
+    
+//     const response: Response  = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     //const body = await response.json();
+//     console.log(await response.json())
+//     setPerson({title:'Tim', userId:1})
+
+//   }
+//   return (
+//     <div>
+//         <button onClick={fetchPerson}>Submit</button>
+//         {person}
+//     </div>
+//   )
+
+// };
+
+// interface Book {
+//   name: string;
+//   index: number;
+// }
+
+// interface User {
+//   gender: string; 
+//   cell: string; 
+// }
+
+// interface ToDo {
+//   userId: number; 
+//   title: string;
+//   completed: boolean; 
+// }
+
+// const App = () => {
+//   const [books, setBooks] = useState<Book[]>([]);
+//   const [user, setUser] = useState<User>({gender:'', cell:''});
+//   const [todo, setTodo] = useState<ToDo>({userId:0, title:'', completed:false})
+//   const [loading, setLoading] = useState(true) 
+
+//   const fetchBooks = async () => {
+//     const response = await fetch('https://www.anapioficeandfire.com/api/books')
+//     const json = await response.json()
+//     setBooks(json);
+//   }
+
+//   const fetchTodo = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     const json = await response.json()
+//     setTodo(json)
+//   }
+
+//   const fetchUser = async () => {
+//     const response = await fetch('https://randomuser.me/api/')
+//     const json = await response.json()
+//     setUser(json.results[0])
+
+//   }
+
+//   const fetchEachAsync = async () => {
+//     const response =  await fetch('https://www.anapioficeandfire.com/api/books')
+//     setBooks(await response.json())
+
+//     const response2 = await fetch('https://randomuser.me/api/')
+//     setUser((await response2.json()).results[0])
+
+//     const response3 = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     setTodo(await response3.json())
+//   }
+
+//   const fetchPromiseAll = async () => {
+//     try {
+//       setBooks([])
+//       const response = fetch('https://www.anapioficeandfire.com/api/books')
+//       const response2 = fetch('https://randomuser.me/api/')
+//       const response3 = fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       const [bookResponse, userResponse, todoResponse] = await Promise.all([response, response2, response3])   
+//       const [bookJSON, userJSON, todoJSON] = await Promise.all([bookResponse.json(), userResponse.json(), todoResponse.json()])
+//       setBooks(bookJSON)
+//       setTodo(todoJSON)
+//       setUser({gender: userJSON.results[0].gender, 
+//               cell: userJSON.results[0].cell})
+//       setLoading(false)
+//     } catch(err) {
+//       console.warn(err)
+//     }
+//   }
+
+//   const fetchPromiseAllCleanedUp = async () => {
+//     try { 
+//       setLoading(true)
+//       await Promise.all([
+//                         fetchTodo(), 
+//                         fetchBooks(), 
+//                         fetchUser()
+//                       ])
+//       setLoading(false)
+//     } catch(err) {
+//       console.error(err)
+//     }
+
+//   }
+
+//   return (
+//     <div className="App">
+
+//     <button onClick={fetchBooks}>Books</button>
+//     <button onClick={fetchTodo}>ToDo</button>
+//     <button onClick={fetchUser}>User</button>
+
+//     <br/>
+//     <br/>
+
+//     <button onClick={fetchEachAsync}>Fetch Each</button>
+//     <button onClick={fetchPromiseAllCleanedUp}>Promise All</button>
+
+//     {books.map((book, index) => {
+//          const indexToDisplay = index += 1;
+//          return <div key={`book${index}`}>{indexToDisplay}&nbsp;{book.name}</div>
+//        })}
+
+//     <br/>
+
+//     { loading ? <a>Loading Component</a> : <a>Not Loading</a> }
+
+//     <a>{user.gender}</a>
+
+//     <br/>
+//     <a>{todo.title}</a>
+
+//     </div>
+//   );
+// };
+
+
+// Fetch the data by making a request 
+// -> what is a request? 
+// what is the api endpoint and what are the parameters ? 
+
+// 'https://randomuser.me/api/'
+// authentication 
+
+// Get the response and process it 
+// what does the api return
+
+
+// Display the data 
+
+// interface Person { 
+//   gender: string;
+//   cell: string; 
+// }
+
+// interface ToDo {
+//   userId: number; 
+//   title: string;
+//   completed: boolean; 
+// }
+
+// interface Book {
+//   url: string;
+//   name: string;
+// }
+
+
+// const App = () => {
+
+//   const [loading, setLoading] = useState(false) 
+//   const [person, setPerson] = useState<Person>({gender:'', cell:''}); 
+//   const [todo, setTodo] = useState<ToDo>({userId:0, title:'', completed:false})
+//   const [books, setBooks] = useState<Book[]>([])
+
+//   const fetchPerson = async () => {
+//     const response = await fetch('https://randomuser.me/api/');
+//     const json = await response.json();
+//     setPerson(json.results[0])
+//   }
+
+//   const fetchTodo = async () => {
+//     setLoading(true)
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     const json = await response.json();
+//     setTodo(json)
+//     setLoading(false)
+//   }
+
+//   const fetchBooks = async () => {
+//     const response = await fetch('https://www.anapioficeandfire.com/api/books')
+//     const json = await response.json();
+//     setBooks(json)
+//   }
+//   const getJson = async (url: RequestInfo) => {
+//     const response = await fetch(url)
+//     return await response.json()
+//   }
+
+//   const fetchAll = async () => {
+//     try { 
+//       setLoading(true)
+//       const [p ,t, b] = await Promise.all([
+//                                         getJson('https://randomuser.me/api/'), 
+//                                         getJson('https://jsonplaceholder.typicode.com/todos/1'),
+//                                         getJson('https://www.anapioficeandfire.com/api/books')
+//                                       ])
+//       setPerson(p.results[0])
+//       setTodo(t) 
+//       setBooks(b)
+//       setLoading(false)      
+//       const newPerson: Person = {gender:p.results[0].gender, cell:p.results[0].cell}
+//       const bs: Book[] = b.map((item: any) => ({'name':item.name, 'url':item.url}))
+//       console.log(bs)
+//     } catch(error) {
+//       console.error(error)
+//     }
+//   }
+
+//   const clear = () => {
+//       setBooks([])
+//       setTodo({userId:0, title:'', completed:false})
+//       setPerson({gender:'', cell:''})
+//   }
+
+//   return (
+//       <div>
+//         <button onClick={clear}>Clear</button>
+//         <button onClick={fetchTodo}>Get Todo</button>
+//         <button onClick={fetchPerson}>Get Person</button>
+//         <button onClick={fetchAll}>Get All</button> 
+//         <br/>
+//           {loading ? (
+//                       <Spinner animation="border" role="status">
+//                       <span className="sr-only">Loading...</span>
+//                       </Spinner>) 
+//                     :  
+//                       <a>{todo.title} {person.gender}</a> 
+//           }
+
+//           { books.length != 0 ? books.map(item => (<a>{item.name}</a>)) : <a></a>}
+
+//       </div>
+
+//   )
+// }
 
 /*
 
 
   client ---- server/api ----- database 
-    <-
+    x
 
 High Level Steps and Concepts   
 
-  1. Send the request for data 
+  1. Send the request to API 
       from client to server  
       client = our users browser
       server = our backend, an api endpoint 
@@ -37,7 +292,7 @@ Scoping the Steps
 Step 1. API Endpoint
   What are our parameters ? 
   What is the end point ? 
-  // https://api.airtable.com/blocks/<id> 
+  // https://api.airtable.com/blocks/<user_id> 
 
 
 Step 2. Response
@@ -51,7 +306,11 @@ interface User {
 }
 
 interface Block {
+  id: number;
+  type: string; 
   data: string; 
+  createdBy: string; 
+  createdOn: string;  
 }
 
 // 3. Authentication
@@ -69,14 +328,19 @@ walk through why this wouldn't work in javascript
   - web apis
   - event queue
   - event loop 
-  - pros: fetch isn't blocking the main thread  
-  - cons: we're going to throw an error 
+  - pros: 
+    - fetch isn't blocking the main thread, we can continue executing the function foo(), bar(), baz() 
+  - cons: 
+    - response.json() depends on response resolving first.  
+    - How can we guarantee? 
 
 introduce Promises/async await keywords 
   - best of both worlds
   - pauses our function on the await line until fetch resolves 
   - doesn't block our main thread so user can use the site 
+  - reads like familiar syncronous code 
 
+                  
 call             fetchBlockData() 
 stack   = [      main()             ] 
  
@@ -86,19 +350,90 @@ event queue = [    fetch()      ]
 
 web apis  = [      ]
 
+
+
+
+client    ------   api    ------   db 
+<-  x 
+
+
+
+1. Send the HTTP request
+
+    client -> user's browser 
+    api -> server 
+
+    talk via http requests 
+      - we will be using a GET request here 
+
+
+2. Receive the response 
+
+    Headers, status codes, 
+    body -> contains the data that we want to display to the user
+    
+
+3. Display the data to the user 
+
+
+Scope
+1. API Signature 
+  - what are the parameters 
+  - what is the endpoint 
+
+  api endpoint: https://www.airtable.com/api/blocks/<userID>
+
+
+2. The Response
+  - What does the body of that response look like? 
+
+
+interface Blocks {
+  userID: number; 
+  blockID: number; 
+  blockType: string; 
+  data: string[];
+  createdOn: string;
+}
+
+
+3. Authentication 
+  - API Key 
+  - passed in query string parameter for GET request
+
+
+
+web apis = [      fetch()        ]
+ 
+
+event  = [              ]
+queue 
+
+event
+loop 
+              
+                
+call            fetchBlocks() 
+stack    = [    main()       ] 
+
 */
+
+
+const fetchBlocks = async (url: any) => {
+  const response = await fetch(url);
+  const json = await response.json(); 
+  return json; 
+}
+
+fetchBlocks() 
 
 
 const App = () => {
 
-  const fetchBlockData = async () => {
-    const response = await fetch('https://randomuser.me/api/')
-    const json = await response.json()
-  }
 
   return (
     <div>
-          <button onClick={fetchBlockData}>This</button>
+          {/* <button onClick={fetchBlockData}>This</button> */}
     </div>
   )
 
